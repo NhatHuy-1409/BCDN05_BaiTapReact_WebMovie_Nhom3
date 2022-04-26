a15c6014d219172ad5ac27f0b349f6692ba8831a
Hieund <ngoduyhieu66@gmail.com>
2022-04-26 10:40:52 +0700

    update home page

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -4,12 +4,13 @@ import QuanLyDatVeReducer from "./reducers/QuanLyDatVeReducer/QuanLyDatVeReducer
 import QuanLyNguoiDungReducer from "./reducers/QuanLyNguoiDungReducer/QuanLyNguoiDungReducer";
 import { CarouselReducer } from "./reducers/CarouselReducer/CarouselReducer";
 import { QuanLyPhimReducer } from "./reducers/QuanLyPhimReducer/QuanLyPhimReducer";
-
+import { QuanLyRapReducer } from "./reducers/QuanLyRapReducer.js/QuanLyRapReducer";
 
 const rootReducer = combineReducers({
   QuanLyNguoiDungReducer,
   QuanLyDatVeReducer,
   CarouselReducer,
   QuanLyPhimReducer,
+  QuanLyRapReducer
 })
 export const store = createStore(rootReducer, applyMiddleware(thunk))
