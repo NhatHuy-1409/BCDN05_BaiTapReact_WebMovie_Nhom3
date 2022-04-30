b7f016715ed22a3283eefe416dde8f0bcfa0eaa6
Quang <tvq.dev@gmail.com>
2022-04-30 18:34:43 +0700

    trang chi tiet

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, CustomCard } from '@tsamantanis/react-glassmorphism';
 import '@tsamantanis/react-glassmorphism/dist/index.css';
 import '../../assets/circle.css';
-import style from'./Detail.module.css'
-import { Tabs, Radio, Space } from 'antd';
-
+import style from './Detail.module.css'
+import { Tabs, Radio, Space, Rate } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { type } from '@testing-library/user-event/dist/type';
+import { SET_CHI_TIET_PHIM } from '../../redux/types/QuanLyRapType';
+import { layThongTinLichChieuPhim } from '../../redux/actions/QuanLyRapAction/QuanLyRapAction';
+import moment from 'moment';
+import { NavLink } from 'react-router-dom';
 const { TabPane } = Tabs;
 
-export default function Detail() {
-
+export default function Detail(props) {
+     const filmDetail = useSelector(state => state.QuanLyPhimReducer.filmDetail);
+     console.log({ filmDetail })
+     console.log(props);
+     const dispatch = useDispatch();
+     useEffect(() => {
+          let { id } = props.match.params;
+          dispatch(layThongTinLichChieuPhim(id))
+     }, [])
      return (
-          <div style={{ backgroundImage: 'url(https://picsum.photos/200/300)', minHeight: '100vh', }}>
+          <div style={{ backgroundImage: `url(${filmDetail.hinhAnh}) `, backgroundSize: '100%', backgroundPosition: 'cover', minHeight: '100vh', }}>
                <CustomCard
                     style={{ paddingTop: 150, minHeight: '100vh' }}
                     effectColor="fff" // required
@@ -20,18 +32,20 @@ export default function Detail() {
                >
                     <div className="grid grid-cols-12">
                          <div className="col-span-4 col-start-4">
-                              <div className="grid-cols-2">
-                                   <img src="https://picsum.photos/200/300" alt="" />
-                                   <div >
-                                        <p>Tên phim</p>
-                                        <p>mô tả</p>
+                              <div className=" grid grid-cols-2">
+                                   <img src={filmDetail.hinhAnh} alt="" />
+                                   <div style={{ marginTop: '30%' }}>
+                                        <p> Ngày Chiếu: {moment(filmDetail.ngayKhoiChieu).format('DD.MM.YYYY')}</p>
+                                        <p className='text-2xl'>{filmDetail.tenPhim}</p>
+                                        <p>{filmDetail.moTa}</p>
                                    </div>
                               </div>
 
                          </div>
-                         <div className="col-span-4">
-                              <div className="c100 p50 big">
-                                   <span>50%</span>
+                         <div className="col-span-5">
+                              <h1 style={{ marginLeft: '5%' }} className="text-green-400 text-2xl"><Rate allowHalf value={filmDetail.danhGia / 2} style={{ color: '#78ed78', fontSize: 30 }}></Rate></h1>
+                              <div className={`c100 p${filmDetail.danhGia * 10} big`}>
+                                   <span>{filmDetail.danhGia * 10}%</span>
                                    <div className="slice">
                                         <div className="bar" />
                                         <div className="fill" />
@@ -40,16 +54,43 @@ export default function Detail() {
                               </div>
                          </div>
                     </div>
-                    <div className='m-10 container'>
-                         <Tabs tabPosition={'right'}>
-                              <TabPane tab="Tab 1" key="1">
-                                   Content of Tab 1
+                    <div className='m-20 ml-72 w-2/3 container px-20 py20'>
+                         <Tabs defaultActiveKey="1" centered>
+                              <TabPane tab="Lịch chiếu" key="1">
+                                   <Tabs tabPosition={'left'}>
+                                        {filmDetail.heThongRapChieu?.map((htr, index) => {
+                                             return <TabPane tab={
+                                                  <div className='flex flex-row items-center justify-center '>
+                                                       <img src={htr.logo} className="rounded-full mr-2" width={50} height={50} alt="" />{htr.tenHeThongRap}
+                                                  </div>} key={index}>
+                                                  {htr.cumRapChieu?.map((cumrap, index) => {
+                                                       return <div key={index}>
+                                                            <div className="flex flex-row">
+                                                                 <img style={{ width: 60, height: 60 }} src="https://movienew.cybersoft.edu.vn/hinhanh/fantasticfour.jpg" alt="" />
+                                                                 <div className="ml-2">
+                                                                      <p style={{ fontSize: 20, lineHeight: 1 }}>{cumrap.tenCumRap}</p>
+                                                                      <p className='text-gray-400' style={{ marginTop: -10 }}>{cumrap.tenCumRap}</p>
+                                                                 </div>
+
+                                                            </div>
+                                                            <div className="thong-tin-lich-chieu grid grid-cols-4 text-white">
+                                                                 {cumrap.lichChieuPhim?.slice(0, 12).map((lichChieu, index) => {
+                                                                      return <NavLink to="/checkout/{maLichChieu}" className="col-span-1" key={index}>
+                                                                           {moment(lichChieu.ngayChieuGioChieu).format('hh:mm A')}
+                                                                      </NavLink>
+                                                                 })}
+                                                            </div>
+                                                       </div>
+                                                  })}
+                                             </TabPane>
+                                        })}
+                                   </Tabs>
                               </TabPane>
-                              <TabPane tab="Tab 2" key="2">
-                                   Content of Tab 2
+                              <TabPane tab="Thông tin" key="2">
+                                   Thông tin
                               </TabPane>
-                              <TabPane tab="Tab 3" key="3">
-                                   Content of Tab 3
+                              <TabPane tab="Đánh giá" key="3">
+                                   Đánh giá
                               </TabPane>
                          </Tabs>
                     </div>
--- src/redux/actions/QuanLyRapAction/QuanLyRapAction.js
+++ src/redux/actions/QuanLyRapAction/QuanLyRapAction.js
@@ -1,20 +1,36 @@
 import { quanLyRapServices } from "../../../services/QuanLyRapServices"
-import { SET_HE_THONG_RAP_CHIEU } from "../../types/QuanLyRapType";
+import { SET_CHI_TIET_PHIM, SET_HE_THONG_RAP_CHIEU } from "../../types/QuanLyRapType";
 
 
-export const layDanhSachHeThongRapAction = () =>{
+export const layDanhSachHeThongRapAction = () => {
   return async dispatch => {
     try {
       const result = await quanLyRapServices.layDanhSachHeThongRap()
       // console.log(result.data);
-      if(result.status === 200){
+      if (result.status === 200) {
         dispatch({
           type: SET_HE_THONG_RAP_CHIEU,
           heThongRapChieu: result.data.content
         })
       }
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
   }
 }
+export const layThongTinLichChieuPhim = (id) => {
+  return async dispatch => {
+    try {
+      const result = await quanLyRapServices.layThongTinLichChieuPhim(id);
+      console.log(result);
+      // lay duoc du lieu tu api => reducer
+      dispatch({
+        type: SET_CHI_TIET_PHIM,
+        filmDetail: result.data.content
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
